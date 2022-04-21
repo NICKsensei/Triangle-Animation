c8d3b86306b03fda933441f66375cb989e28e581
NICKsensei <refat.rar@gmail.com>
2022-04-21 20:47:11 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+import random
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+triad = turtle.Turtle()
+triad.speed(0)
+triad.up()
+triad.goto(0, 0)
+triad.down()
+colors = ["red", "gold", "blue", "green", "white", "cyan", "pink"]
+
+for i in range(100):
+    triad.color(random.choice(colors))
+    triad.forward(i*3)
+    print(i*3)
+    triad.left(90)
+triad.down()
+triad.goto(-110, 200)
+triad.down()
+triad.hideturtle()
+turtle.done()
